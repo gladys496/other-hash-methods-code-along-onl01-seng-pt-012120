8a83e7c616baca52794e980764c13cd2fe722363
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-03 11:55:50 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -9,12 +9,5 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-letters = {a: 1, b: 2}
- 
-letters.min
-#  => [:a, 1]
- 
-alphabetical_order = {c: 100, d: 1}
-alphabetical_order.min
-#  => [c:, 100] # Because the key :c is lower than :d in alphabetical order.
+  groceries.values.flatten.min
 end
